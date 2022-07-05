086bd9c22f3eb76a0ec181f2ce9911fc58eeab82
伊東聖矢 <itoseiya1219@gmail.com>
2022-07-05 15:19:45 +0900

    fg描画

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@ function drawBg(ctx) {
     let img = new Image();
     img.onload = function () {
         ctx.drawImage(img, 0, 0, 1250, 750);
+        drawFg(ctx);
     };
     img.src = './images/bg.png';
 }
+
+function drawFg(ctx) {
+    let img = new Image();
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, 1250, 125);
+    };
+    img.src = './images/fg.png';
+}
+
