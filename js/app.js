0eed55a2105288618b90f1cca42ff23f3423f562
伊東聖矢 <itoseiya1219@gmail.com>
2022-07-19 13:55:50 +0900

    wip

--- js/app.js
+++ js/app.js
@@ -1,26 +1,38 @@
-function draw() {
+// キャンバスを描画
+draw = () => {
     let canvas = document.getElementById('canvas');
 
     if (canvas.getContext) {
         let ctx = canvas.getContext('2d');
         drawBg(ctx);
-    }
-}
+    };
+};
 
-function drawBg(ctx) {
+// 背景を描画
+drawBg = (ctx) => {
     let img = new Image();
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, 1250, 750);
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, 1024, 576);
         drawFg(ctx);
     };
     img.src = './images/bg.png';
-}
+};
 
-function drawFg(ctx) {
+// 前景を描画
+drawFg = (ctx) => {
     let img = new Image();
-    img.onload = function () {
-        ctx.drawImage(img, 0, 600, 1250, 125);
+    img.onload = () => {
+        ctx.drawImage(img, 0,480, 1024, 120);
+        drawPipe(ctx);
     };
     img.src = './images/fg.png';
-}
+};
 
+// パイプ(障害物)を描画
+drawPipe = (ctx) => {
+    let img = new Image();
+    img.onload = () => {
+        ctx.drawImage(img, 500, 400);
+    };
+    img.src = './images/pipeUp.png';
+};
