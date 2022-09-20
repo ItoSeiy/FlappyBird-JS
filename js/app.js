392cb3fd765bd545e6c3067efbd385250aefe42b
伊東聖矢 <itoseiya1219@gmail.com>
2022-09-20 15:00:22 +0900

    障害物のランダム描画 wip

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
+let bgSrc = "./images/bg.png";
+let fgSrc = "./images/fg.png";
+let pipeUp = "./images/pipeUp.png";
+let pipeDown = "./images/pipeDown.png";
+
+let pipeInitialDx = 500;
+
 // キャンバスを描画
 draw = () => {
-    let canvas = document.getElementById('canvas');
+    let canvas = document.getElementById("canvas");
 
     if (canvas.getContext) {
-        let ctx = canvas.getContext('2d');
+        let ctx = canvas.getContext("2d");
         drawBg(ctx);
     };
 };
@@ -11,28 +18,35 @@ draw = () => {
 // 背景を描画
 drawBg = (ctx) => {
     let img = new Image();
+    img.src = bgSrc;
     img.onload = () => {
         ctx.drawImage(img, 0, 0, 1024, 576);
         drawFg(ctx);
     };
-    img.src = './images/bg.png';
 };
 
 // 前景を描画
 drawFg = (ctx) => {
     let img = new Image();
+    img.src = fgSrc;
     img.onload = () => {
         ctx.drawImage(img, 0,480, 1024, 120);
-        drawPipe(ctx);
+        drawPipeRandom(ctx);
     };
-    img.src = './images/fg.png';
 };
 
-// パイプ(障害物)を描画
-drawPipe = (ctx) => {
-    let img = new Image();
-    img.onload = () => {
-        ctx.drawImage(img, 500, 400);
+// パイプ(障害物)をランダムに描画
+drawPipeRandom = (ctx) => {
+    let pipes = [];
+    pipes.push(new Image(), new Image());
+    pipes[0].src = pipeUp;
+    pipes[1].src = pipeDown;
+
+    pipes[0].onload = () => {
+        ctx.drawImage(pipes[0], pipeInitialDx, 350);
     };
-    img.src = './images/pipeUp.png';
+
+    pipes[1].onload = () => {
+        ctx.drawImage(pipes[1], pipeInitialDx, 0);
+    };
 };
