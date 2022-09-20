333d51b09975d53f95fc7e44eee90ebb708c2cb2
伊東聖矢 <itoseiya1219@gmail.com>
2022-09-20 17:01:51 +0900

    パイプランダム生成完了

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ let pipeUp = "./images/pipeUp.png";
 let pipeDown = "./images/pipeDown.png";
 
 let pipeInitialDx = 500;
+let pipeYAxisMax = 400;
+let pipeYAxisMin = 230;
+let pipeSpace = 400;
 
 // キャンバスを描画
 draw = () => {
@@ -12,8 +15,8 @@ draw = () => {
     if (canvas.getContext) {
         let ctx = canvas.getContext("2d");
         drawBg(ctx);
-    };
-};
+    }
+}
 
 // 背景を描画
 drawBg = (ctx) => {
@@ -22,8 +25,8 @@ drawBg = (ctx) => {
     img.onload = () => {
         ctx.drawImage(img, 0, 0, 1024, 576);
         drawFg(ctx);
-    };
-};
+    }
+}
 
 // 前景を描画
 drawFg = (ctx) => {
@@ -32,8 +35,8 @@ drawFg = (ctx) => {
     img.onload = () => {
         ctx.drawImage(img, 0,480, 1024, 120);
         drawPipeRandom(ctx);
-    };
-};
+    }
+}
 
 // パイプ(障害物)をランダムに描画
 drawPipeRandom = (ctx) => {
@@ -41,12 +44,18 @@ drawPipeRandom = (ctx) => {
     pipes.push(new Image(), new Image());
     pipes[0].src = pipeUp;
     pipes[1].src = pipeDown;
-
+    let yAxis = getRandomYAxes();
+    console.log(yAxis[0], yAxis[1]);
     pipes[0].onload = () => {
-        ctx.drawImage(pipes[0], pipeInitialDx, 350);
+        ctx.drawImage(pipes[0], pipeInitialDx, yAxis[0]);
     };
 
     pipes[1].onload = () => {
-        ctx.drawImage(pipes[1], pipeInitialDx, 0);
-    };
-};
+        ctx.drawImage(pipes[1], pipeInitialDx, yAxis[1]);
+    }
+}
+
+getRandomYAxes = () =>{
+    let yAxis = Math.floor(Math.random() * (pipeYAxisMax - pipeYAxisMin) + pipeYAxisMin);
+    return [yAxis, (yAxis - pipeSpace)];
+}
